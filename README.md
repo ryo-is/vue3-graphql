9b8c66be6c5495ce1d77474796a1651a1345a348
Ryosuke Izumi <ryosuke.izumi62@gmail.com>
2020-12-18 16:35:21 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Start TypeScript
+
+Let's Start TypeScriptのソースコード
+
+## db
+
+PostgreSQLのDocker環境
+
+## front-app
+
+フロントエンドのソースコード
+
+## server
+
+バックエンドのソースコード
---
+++ db/migrations/20201021144816-create-todo-task.js
@@ -0,0 +1,32 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('TodoTasks', {
+      uuid: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      status: {
+        allowNull: false,
+        defaultValue: 'todo',
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('TodoTasks');
+  }
+};
